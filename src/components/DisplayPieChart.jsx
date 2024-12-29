6b1e0dc216ac0578c624be28a0f6d299cb423f16
skidyuk8615 <chyu23072@g.nihon-u.ac.jp>
2024-12-29 15:23:23 +0900

    グラフの始まりを上に、時計回りに変更

--- src/components/DisplayPieChart.jsx
+++ src/components/DisplayPieChart.jsx
@@ -1,15 +1,22 @@
-import { PieChart, Pie, } from 'recharts';
+import { PieChart, Pie } from 'recharts';
 
-const DisplayPieChart = ({ data }) => {
-  //console.log(data);
+export default function DisplayPieChart({ data }) {
   return (
     <div>
       <h2>ゲームプレイ時間Top10</h2>
       <PieChart width={1000} height={400}>
-        <Pie data={data} dataKey="playtime" nameKey="name" cx="50%" cy="50%" outerRadius={150} label={({ name }) => name} />
+        <Pie
+          data={data}
+          dataKey="playtime"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius={150}
+          startAngle={90}
+          endAngle={-270}
+          label={({ name }) => name}
+        />
       </PieChart>
     </div>
   );
 };
-
-export default DisplayPieChart;
